56858c9d16f600f82dc1733e0da7c65a2f46dcff
Kate Konyahina <ekaterina.konyahina98@gmail.com>
2022-06-25 11:47:04 +0300

    Add HW.01, HW.02, HW.03

---
+++ CSharp.Homeworks/HW.03.QuadraticFormula/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Найте корни уравнения ax^2 + bx + c = 0");
+
+Console.WriteLine("Ввeдите значение аргументa a и нажмите Enter");
+ string inp1 = Console.ReadLine();
+ int a = Int32.Parse(inp1);
+
+Console.WriteLine("Ввeдите значение аргументa b и нажмите Enter");
+string inp2 = Console.ReadLine();
+int b = Int32.Parse(inp2);
+
+Console.WriteLine("Ввeдите значение аргументa c и нажмите Enter");
+string inp3 = Console.ReadLine();
+int c = Int32.Parse(inp3);
+
+//найдем дискриминант уравнения
+int discr = b * b - 4 * a * c;
+
+//проверка на наличие корней
+if (discr >= 0)
+{
+    double x1 = (-b + Math.Sqrt(discr)) / (2 * a);
+    double x2 = (-b - Math.Sqrt(discr)) / (2 * a);
+
+    Console.WriteLine("x1 = " + x1);
+    Console.WriteLine("x2 = " + x2);
+}
+else
+    Console.WriteLine("Уравнение не имеет корней.");
+
---
+++ CSharp.Homeworks/HW.04.Image/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+StreamReader textReader = new StreamReader(@"C:\Users\user\source\repos\kate-konyahina\CSharp.Homeworks\CSharp.Homeworks\HW.04.Image\image.txt", true);
+textReader.Dispose();
+string textReaderResult = textReader.ReadToEnd();
+string arrayOfTextResult = textReaderResult.Split(' ');
+byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+{
+
+}
+byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+imageBytes[i] = binary;
+File.WriteAllBytes(@"C:\Users\user\source\repos\kate-konyahina\CSharp.Homeworks\CSharp.Homeworks\HW.04.Image\image.png", imageBytes);
