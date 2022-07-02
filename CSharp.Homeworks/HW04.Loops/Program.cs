8e08254bccd69adf86b8f49ea2729249050d47a1
Kate Konyahina <ekaterina.konyahina98@gmail.com>
2022-07-02 13:43:21 +0300

    Addes HWo4

---
+++ CSharp.Homeworks/HW04.Loops/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for (int i = 122; i >= 97; --i)
+{ char c = Convert.ToChar(i);
+    Console.Write(c);
+}   
---
+++ CSharp.Homeworks/HW04.Operators4/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Repeat1:
+Console.WriteLine("Введите первое число:");
+string inp1 = Console.ReadLine();
+bool success1 = int.TryParse(inp1, out int num1);
+if (success1 == false)
+{
+    Console.WriteLine("Введенный символ не является числом.");
+    goto Repeat1;
+}
+
+Repeat2:
+Console.WriteLine("Введите второе число:");
+string inp2 = Console.ReadLine();
+bool success2 = int.TryParse(inp2, out int num2);
+if (success2 == false)
+{
+    Console.WriteLine("Введенный символ не является числом.");
+    goto Repeat2;
+}
+
+Repeat5:
+Console.WriteLine("Какой оператор использовать для вычислений? (+ или -)");
+string op = Console.ReadLine();
+if (op == "+")
+{
+Repeat3:
+    Console.WriteLine("Чему равна сумма введеннных чисел?");
+    string inp3 = Console.ReadLine();
+    bool success3 = int.TryParse(inp3, out int sum);
+    if (success3 == false)
+    {
+        Console.WriteLine("Введенный символ не является числом.");
+        goto Repeat3;
+    }
+
+    if (sum == num1 + num2)
+    {
+        Console.WriteLine("Правильно");
+    }
+    else if (sum > num1 + num2)
+    {
+        Console.WriteLine("Неправильно");
+        Console.WriteLine("Должно быть меньше");
+    }
+    else
+    {
+        Console.WriteLine("Неправильно");
+        Console.WriteLine("Должно быть больше");
+    }
+}
+else if (op == "-")
+{
+Repeat4:
+    Console.WriteLine("Чему равна разность введеннных чисел?");
+    string inp4 = Console.ReadLine();
+    bool success4 = int.TryParse(inp4, out int dif);
+    if (success4 == false)
+    {
+        Console.WriteLine("Введенный символ не является числом.");
+        goto Repeat4;
+    }
+
+    if (dif == num1 - num2)
+    {
+        Console.WriteLine("Правильно");
+    }
+    else if (dif > num1 - num2)
+    {
+        Console.WriteLine("Неправильно");
+        Console.WriteLine("Должно быть меньше");
+    }
+    else
+    {
+        Console.WriteLine("Неправильно");
+        Console.WriteLine("Должно быть больше");
+    }
+}
+else
+{
+    Console.WriteLine("Введен неверный оператор.");
+    goto Repeat5;
+}
