19f4341a3d2f24b9a8f2b57cc1667dedbdf55745
Kate Konyahina <ekaterina.konyahina98@gmail.com>
2022-07-02 13:43:21 +0300

    Added HW04

---
+++ CSharp.Homeworks/HW04.Game/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+char key = char.ToLower(Console.ReadKey(true).KeyChar);
+switch (key)
+{
+    case (char)97:
+        Console.WriteLine("Движение влево");
+        break;
+
+    case (char)119:
+        Console.WriteLine("Движение вверх");
+        break;
+
+    case (char)115:
+        Console.WriteLine("Движение вниз");
+        break;
+
+    case (char)100:
+        Console.WriteLine("Движение вправо");
+        break;
+
+        default:
+        Console.WriteLine("Необходимость перемещения отсутствует");
+        break;
+}
+
