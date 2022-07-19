129afb1b3a82bd38f0e7ad7ec430549b3db9154f
Kate Konyahina <ekaterina.konyahina98@gmail.com>
2022-07-19 13:28:36 +0300

    Add HW.07.Task4

---
+++ CSharp.Homeworks/HW.07.Task4.TravelingSalesman/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Insert adresses: ");
+StringBuilder adressesStr = new StringBuilder(Console.ReadLine());
+string[] adresses = (adressesStr.ToString()).Split(',');
+
